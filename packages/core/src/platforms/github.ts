99c90b0ce9aac4f2546278acd709bf5d00196677
ikeshav26 <keshavgilhotra4@gmail.com>
2025-12-25 19:02:15 +0530

    applicable for repos, PR's and blobs too

--- packages/core/src/platforms/github.ts
+++ packages/core/src/platforms/github.ts
@@ -1,14 +1,49 @@
 import { DeepLinkHandler } from '../types';
 
 export const githubHandler: DeepLinkHandler = {
-  match: (url) => url.match(/github\.com\/([^\/\?#]+)/),
+  match: (url) => url.match(/github\.com\/([^\/\?#]+)(?:\/([^\/\?#]+)(?:\/(pull|blob)\/([^\?#]+))?)?/),
 
   build: (webUrl, match) => {
     const owner = match[1];
+    const repo = match[2];
+    const type = match[3];
+    const remainder = match[4];
 
+    // PR's and Blobs
+    if (repo && type && remainder) {
+      if (type === 'pull') {
+        return {
+          webUrl,
+          ios: `github://repo/${owner}/${repo}/pull/${remainder}`,
+          android: `intent://github.com/${owner}/${repo}/pull/${remainder}#Intent;scheme=https;package=com.github.android;end`,
+          platform: 'github',
+        };
+      }
+      
+      if (type === 'blob') {
+        return {
+          webUrl,
+          ios: `github://repo/${owner}/${repo}/blob/${remainder}`,
+          android: `intent://github.com/${owner}/${repo}/blob/${remainder}#Intent;scheme=https;package=com.github.android;end`,
+          platform: 'github',
+        };
+      }
+    }
+
+    // repo
+    if (repo) {
+      return {
+        webUrl,
+        ios: `github://repo/${owner}/${repo}`,
+        android: `intent://github.com/${owner}/${repo}#Intent;scheme=https;package=com.github.android;end`,
+        platform: 'github',
+      };
+    }
+
+    // profile
     return {
       webUrl,
-      ios: `github:///user/${owner}`,
+      ios: `github://user/${owner}`,
       android: `intent://github.com/${owner}#Intent;scheme=https;package=com.github.android;end`,
       platform: 'github',
     };
